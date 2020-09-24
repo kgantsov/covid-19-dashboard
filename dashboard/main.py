0dc2ea796fb9f7a40aff5357b6d50c31ea369756
Kostiantyn Hantsov <k.gantsov@gmail.com>
2020-09-24 19:00:39 +0200

    Fix styles

--- dashboard/main.py
+++ dashboard/main.py
@@ -22,7 +22,7 @@ covid19 = Covid19Data()
 
 external_stylesheets = [
     # 'https://codepen.io/chriddyp/pen/bWLwgP.css',
-    'https://codepen.io/kgantsov/pen/jOPROez.css?v=4'
+    'https://gist.githubusercontent.com/kgantsov/cd2985e4126dfe275946db59c5c2cb23/raw/6aa3c769fd76862bc623ba742284a94d48d90328/style.css?v=4'
 ]
 google_analytics_id = os.environ.get('GOOGLE_ANALYTICS_ID')
 if google_analytics_id:
